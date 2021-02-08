a534848d4ce07ee603aa1819c0a7f2f556e8dddb
shalyganov <shalyganov@haulmont.com>
2021-02-08 15:02:27 +0400

    Migrate AWS file storage Haulmont/jmix-awsfs#1

--- clone-subprojects.bat
+++ clone-subprojects.bat
@@ -9,6 +9,7 @@ git clone https://github.com/Haulmont/jmix-dynattr.git
 git clone https://github.com/Haulmont/jmix-email.git
 git clone https://github.com/Haulmont/jmix-graphql.git
 git clone https://github.com/Haulmont/jmix-localfs.git
+git clone https://github.com/Haulmont/jmix-awsfs.git
 git clone https://github.com/Haulmont/jmix-gradle-plugin.git
 git clone https://github.com/Haulmont/jmix-grapesjs.git
 git clone https://github.com/Haulmont/jmix-imap.git
